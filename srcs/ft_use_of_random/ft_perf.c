11b4da660acdf313b23e5790c246d41511d6648d
Alexdelia <alexandre.delille.57@gmail.com>
2021-02-17 23:53:35 +0100

    very happy of what the project has become

--- srcs/ft_use_of_random/ft_perf.c
+++ srcs/ft_use_of_random/ft_perf.c
@@ -6,7 +6,7 @@
 /*   By: user42 <adelille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 21:14:52 by user42            #+#    #+#             */
-/*   Updated: 2021/02/17 23:39:58 by user42           ###   ########.fr       */
+/*   Updated: 2021/02/17 23:52:44 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ static t_map	ft_best_map_perf(t_map *map)
 					printf("\rLoop: [%d%%]", (i * 100) / 5000);
 				else if (bol == FALSE)
 				{
-					printf("\r\t\tFinishing, please wait a few minute.");
+					ft_putstr_fd("\r\t\tFinishing, please wait a few minute.", 1);
 					bol = TRUE;
 				}
 				// Help to erase about 3000 loops in average
@@ -71,7 +71,7 @@ static t_map	ft_best_map_perf(t_map *map)
 		i++;
 	}
 	printf("\rloop: [Done]");
-	printf("\nnumber of loop: %d, for a maximum of 20417 loops", i);
+	printf("\nnumber of loops: %d, for a maximum of 20417 loops", i);
 	return (best);
 }
 
@@ -87,7 +87,7 @@ int				ft_map_perf(t_arg arg)
 	ft_putstr_fd("\t\tStarting LCG performance\n", 1);
 	map = ft_best_map_perf(&map);
 	printf("\nThe best LCG found is:\n");
-	printf("\n\tMul: %llu\n\tInc: %llu\n\tMod: %llu\n\tPeriod: %llu\n\n",
+	printf("\n\t   Mul:\t%llu\n\t   Inc:\t%llu\n\t   Mod:\t%llu\n\tPeriod:\t%llu\n\n",
 				map.mul, map.inc, map.mod, map.period);
 	printf("For a Score of: %llu%%\n", (map.period + 1) * 100 / map.mod);
 	if (arg.debug.bol == TRUE)
